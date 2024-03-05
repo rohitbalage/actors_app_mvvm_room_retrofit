9436b2b221c1901c928c953f220cbe363b3be679
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-05 06:07:52 -0500

    added repository

---
+++ app/src/main/java/com/rrbofficial/viewmodel/CharacterViewModel.kt
@@ -0,0 +1,25 @@
+package com.rrbofficial.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rrbofficial.repository.CharacterRepo
+import com.rrbofficial.retrofit.Character
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class CharacterViewModel (val characterRepo: CharacterRepo) : ViewModel (){
+
+    private  val _state = MutableStateFlow(emptyList<Character>())
+    val state: StateFlow<List<Character>>
+        get() = _state
+
+    init {
+        viewModelScope.launch {
+
+            val characters = characterRepo.getCharacters()
+            _state.value = characters
+        }
+    }
+
+}
