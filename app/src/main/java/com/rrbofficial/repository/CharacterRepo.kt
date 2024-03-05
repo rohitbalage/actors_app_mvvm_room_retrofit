c354683a556337b187299d3d204078c24cacfced
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-05 05:57:33 -0500

    done with remote data source and repository in MODEL

--- app/src/main/java/com/rrbofficial/repository/CharacterRepo.kt
+++ app/src/main/java/com/rrbofficial/repository/CharacterRepo.kt
@@ -1,4 +1,19 @@
 package com.rrbofficial.repository
 
-class CharacterRepo {
+import com.rrbofficial.retrofit.Character
+import com.rrbofficial.retrofit.CharacterAPi
+
+class CharacterRepo(private val characterApi: CharacterAPi) {
+    // Repository can gather data from different data source
+    // different REST APIs, cache, local database storage
+    // and it provides this data to the rest of the app.
+
+
+    suspend fun  getCharacters() : List<Character>
+    {
+    return characterApi.getCharacters()
+    }
+
+
+
 }
